9a424b04cddf8f5392d16cbc4f33440f9d5380aa
William Volin <me@williamvolin.com>
2022-12-21 15:24:07 -0700

    use ueberauth 0.10

--- mix.exs
+++ mix.exs
@@ -6,7 +6,6 @@ defmodule UeberauthTwitch.MixProject do
       app: :ueberauth_twitch,
       version: "0.1.0",
       build_path: "../../_build",
-      config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
       elixir: "~> 1.11",
@@ -26,7 +25,7 @@ defmodule UeberauthTwitch.MixProject do
   defp deps do
     [
       {:oauth2, "~> 2.0"},
-      {:ueberauth, "~> 0.6.3"},
+      {:ueberauth, "~> 0.10.3"},
       {:credo, "~> 1.5.0-rc.2", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
